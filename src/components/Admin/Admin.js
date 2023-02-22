91d261cd707d9d5f786cbdae09229a0f276f2d58
Darshana Gunawardana <darshana@Darshanas-MacBook-Pro.local>
2023-02-22 22:04:35 +0530

    Add product impl

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Button, Table }  from 'react-bootstrap';
+import { Container, Button, Table, Modal, Form }  from 'react-bootstrap';
 import { useAuthContext } from "@asgardeo/auth-react";
 
 export default function Admin() {
@@ -62,6 +62,42 @@ export default function Admin() {
             .catch((error) => console.error("API call failed:", error));
     };
 
+
+  const [showModal, setShowModal] = useState(false);
+  const [formValues, setFormValues] = useState({ name: "", price: 0});
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+
+  const handleAddProduct = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setFormValues({ name: "", price: 0 });
+  };
+
+  const handleSubmit = (event, title, price) => {
+    event.preventDefault();
+    console.log(`Adding product with name title ${title}, and price ${price}`);
+  
+
+    const url = "https://a4bcf7b0-79b1-4ffb-99cc-dbbaee233af5-prod.e1-us-east-azure.choreoapis.dev/bilp/catelog-admin/1.0.0/items";
+    const payload = `{ "name": "${title}", "price": ${price} }`;
+
+    httpRequest({
+      url,
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      data: payload,
+    })
+      .then((response) => {
+        console.log("API call successful:", response);
+        handleCloseModal(); // Close the modal after the API call
+        fetchData(); // Refresh the data after the API call
+      })
+      .catch((error) => console.error("API call failed:", error));
+  };
     return (
         <>
         <Container className="mt-5">
@@ -116,11 +152,52 @@ export default function Admin() {
                         </tr>
                     ))}
                     <tr className="text-end">
-                        <td colSpan="8"><Button variant="primary" className="float-right">Add New Product</Button></td>
+                        <td colSpan="8">
+                            <Button
+                                    variant="primary"
+                                    className="float-right"
+                                    onClick={() => handleAddProduct()}
+                                >
+                                    Add New Product
+                                </Button>
+                        </td>
                     </tr>
                 </thead>
             </Table>
-        </Container>
+
+                <Modal
+                    show={showModal}
+                    onHide={() => setShowModal(false)}
+                    backdrop="static"
+                    keyboard={false}
+                >
+                    <Modal.Header closeButton>
+                        <Modal.Title>Add New Product</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form onSubmit={(e) => handleSubmit(e, title, price)}>
+                            <Form.Group controlId="title">
+                                <Form.Label>Title</Form.Label>
+                                <Form.Control type="text" placeholder="Enter title" onChange={(e) => setTitle(e.target.value)} />
+                            </Form.Group>
+                            {/* <Form.Group controlId="description">
+                                <Form.Label>Description</Form.Label>
+                                <Form.Control type="email" placeholder="Enter description" />
+                            </Form.Group> */}
+                            <Form.Group controlId="price">
+                                <Form.Label>Price</Form.Label>
+                                <Form.Control type="text" placeholder="Enter price" onChange={(e) => setPrice(e.target.value)} />
+                            </Form.Group>
+                            {/* <Button variant="primary" type="submit" onClick={() => handleAddProduct()}>
+                                Submit
+                            </Button> */}
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Form>
+                    </Modal.Body>
+                </Modal>
+            </Container>
         </>
     );
 }
